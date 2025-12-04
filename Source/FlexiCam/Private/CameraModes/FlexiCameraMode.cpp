b9969f8a24f4f94efe3eab86bde572950c4644fb
egorkapk <ermakovne@mail.ru>
2025-12-05 00:08:34 +0300

    Initial commit

---
+++ Source/FlexiCam/Private/CameraModes/FlexiCameraMode.cpp
@@ -0,0 +1,450 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CameraModes/FlexiCameraMode.h"
+
+#include "GameFramework/Character.h"
+#include "Components/CapsuleComponent.h"
+
+// CameraModeView
+
+FCameraModeView::FCameraModeView() :
+	Location(ForceInit),
+	Rotation(ForceInit),
+	ControlRotation(ForceInit),
+	FieldOfView(90) // TODO: make FOV setting
+{
+
+}
+
+void FCameraModeView::Blend(const FCameraModeView& Other, float OtherWeight)
+{
+	// Process blending edge cases
+	if (OtherWeight <= 0.0f)
+	{
+		return;
+	}
+	else if (OtherWeight >= 1.0f)
+	{
+		*this = Other;
+		return;
+	}
+
+	Location = FMath::Lerp(Location, Other.Location, OtherWeight);
+
+	const FRotator DeltaRotation = (Other.Rotation - Rotation).GetNormalized();
+	Rotation = Rotation + (OtherWeight * DeltaRotation);
+
+	const FRotator DeltaControlRotation = (Other.ControlRotation - ControlRotation).GetNormalized();
+	ControlRotation = ControlRotation + (OtherWeight * DeltaControlRotation);
+
+	FieldOfView = FMath::Lerp(FieldOfView, Other.FieldOfView, OtherWeight);
+}
+
+// FlexiCameraMode
+
+UFlexiCameraMode::UFlexiCameraMode()
+{
+	// TODO: Create defaults settings
+	// Set defaults settings 
+	FieldOfView = 90;
+	ViewPitchMin = -90;
+	ViewPitchMax = 90;
+
+	BlendTime = 0.5f;
+	BlendFunction = ECameraModeBlendFunction::EaseOut;
+	BlendExponent = 4.0f;
+	BlendAlpha = 1.0f;
+	BlendWeight = 1.0f;
+}
+
+UE_API UFlexiCameraComponent* UFlexiCameraMode::GetFlexiCameraComponent() const
+{
+	// TODO: Implement FlexiCameraComponent
+	return nullptr;
+}
+
+UE_API UWorld* UFlexiCameraMode::GetWorld() const
+{
+	// This class is used as abstract template. So we dont have Outer here and have to create childs.
+	return HasAnyFlags(RF_ClassDefaultObject) ? nullptr : GetOuter()->GetWorld();
+}
+
+UE_API AActor* UFlexiCameraMode::GetTargetActor() const
+{
+	// TODO:Implement
+	return nullptr;
+}
+
+UE_API void UFlexiCameraMode::UpdateCameraMode(float DeltaTime)
+{
+	UpdateView(DeltaTime);
+	UpdateBlending(DeltaTime);
+}
+
+UE_API void UFlexiCameraMode::SetBlendWeight(float Weight)
+{
+	BlendWeight = FMath::Clamp(Weight, 0.0f, 1.0f);
+
+	// Since we're setting the blend weight directly, we need to calculate the blend alpha to account for the blend function.
+	const float InvExponent = (BlendExponent > 0.0f) ? (1.0f / BlendExponent) : 1.0f;
+
+	switch (BlendFunction)
+	{
+	case ECameraModeBlendFunction::Linear:
+		BlendAlpha = BlendWeight;
+		break;
+
+	case ECameraModeBlendFunction::EaseIn:
+		BlendAlpha = FMath::InterpEaseIn(0.0f, 1.0f, BlendWeight, InvExponent);
+		break;
+
+	case ECameraModeBlendFunction::EaseOut:
+		BlendAlpha = FMath::InterpEaseOut(0.0f, 1.0f, BlendWeight, InvExponent);
+		break;
+
+	case ECameraModeBlendFunction::EaseInOut:
+		BlendAlpha = FMath::InterpEaseInOut(0.0f, 1.0f, BlendWeight, InvExponent);
+		break;
+
+	default:
+		checkf(false, TEXT("SetBlendWeight: Invalid BlendFunction [%d]\n"), (uint8)BlendFunction);
+		break;
+	}
+}
+
+const FCameraModeView& UFlexiCameraMode::GetCameraModeView() const
+{
+	return View;
+}
+
+float UFlexiCameraMode::GetBlendTime() const
+{
+	return BlendTime;
+}
+
+float UFlexiCameraMode::GetBlendWeight() const
+{
+	return BlendWeight;
+}
+
+FGameplayTag UFlexiCameraMode::GetCameraTypeTag() const
+{
+	return CameraTypeTag;
+}
+
+UE_API FVector UFlexiCameraMode::GetPivotLocation() const
+{
+	const AActor* TargetActor = GetTargetActor();
+	check(TargetActor);
+
+	if (const APawn* TargetPawn = Cast<APawn>(TargetActor))
+	{
+		// Height adjustments for characters to account for crouching.
+		if (const ACharacter* TargetCharacter = Cast<ACharacter>(TargetPawn))
+		{
+			const ACharacter* TargetCharacterCDO = TargetCharacter->GetClass()->GetDefaultObject<ACharacter>();
+			check(TargetCharacterCDO);
+
+			const UCapsuleComponent* CapsuleComp = TargetCharacter->GetCapsuleComponent();
+			check(CapsuleComp);
+
+			const UCapsuleComponent* CapsuleCompCDO = TargetCharacterCDO->GetCapsuleComponent();
+			check(CapsuleCompCDO);
+
+			const float DefaultHalfHeight = CapsuleCompCDO->GetUnscaledCapsuleHalfHeight();
+			const float ActualHalfHeight = CapsuleComp->GetUnscaledCapsuleHalfHeight();
+			const float HeightAdjustment = (DefaultHalfHeight - ActualHalfHeight) + TargetCharacterCDO->BaseEyeHeight;
+
+			return TargetCharacter->GetActorLocation() + (FVector::UpVector * HeightAdjustment);
+		}
+
+		return TargetPawn->GetPawnViewLocation();
+	}
+
+	return TargetActor->GetActorLocation();
+}
+
+UE_API FRotator UFlexiCameraMode::GetPivotRotation() const
+{
+	const AActor* TargetActor = GetTargetActor();
+	check(TargetActor);
+
+	if (const APawn* TargetPawn = Cast<APawn>(TargetActor))
+	{
+		return TargetPawn->GetViewRotation();
+	}
+
+	return TargetActor->GetActorRotation();
+}
+
+UE_API void UFlexiCameraMode::UpdateView(float DeltaTime)
+{
+	FVector PivotLocation = GetPivotLocation();
+	FRotator PivotRotation = GetPivotRotation();
+
+	PivotRotation.Pitch = FMath::ClampAngle(PivotRotation.Pitch, ViewPitchMin, ViewPitchMax);
+
+	View.Location = PivotLocation;
+	View.Rotation = PivotRotation;
+	View.ControlRotation = View.Rotation;
+	View.FieldOfView = FieldOfView;
+}
+
+UE_API void UFlexiCameraMode::UpdateBlending(float DeltaTime)
+{
+	if (BlendTime > 0.0f)
+	{
+		BlendAlpha += (DeltaTime / BlendTime);
+		BlendAlpha = FMath::Min(BlendAlpha, 1.0f);
+	}
+	else
+	{
+		BlendAlpha = 1.0f;
+	}
+
+	const float Exponent = (BlendExponent > 0.0f) ? BlendExponent : 1.0f;
+
+	switch (BlendFunction)
+	{
+	case ECameraModeBlendFunction::Linear:
+		BlendWeight = BlendAlpha;
+		break;
+
+	case ECameraModeBlendFunction::EaseIn:
+		BlendWeight = FMath::InterpEaseIn(0.0f, 1.0f, BlendAlpha, Exponent);
+		break;
+
+	case ECameraModeBlendFunction::EaseOut:
+		BlendWeight = FMath::InterpEaseOut(0.0f, 1.0f, BlendAlpha, Exponent);
+		break;
+
+	case ECameraModeBlendFunction::EaseInOut:
+		BlendWeight = FMath::InterpEaseInOut(0.0f, 1.0f, BlendAlpha, Exponent);
+		break;
+
+	default:
+		checkf(false, TEXT("UpdateBlending: Invalid BlendFunction [%d]\n"), (uint8)BlendFunction);
+		break;
+	}
+}
+
+// FlexiCameraMode Stack
+
+UFlexiCameraModeStack::UFlexiCameraModeStack()
+{
+	bIsActive = true;
+}
+
+void UFlexiCameraModeStack::ActivateStack()
+{
+	if (!bIsActive)
+	{
+		bIsActive = true;
+
+		// Notify camera modes that they are being activated.
+		for (UFlexiCameraMode* CameraMode : CameraModeStack)
+		{
+			check(CameraMode);
+			CameraMode->OnActivation();
+		}
+	}
+}
+
+void UFlexiCameraModeStack::DeactivateStack()
+{
+	if (bIsActive)
+	{
+		bIsActive = false;
+
+		// Notify camera modes that they are being deactivated.
+		for (UFlexiCameraMode* CameraMode : CameraModeStack)
+		{
+			check(CameraMode);
+			CameraMode->OnDeactivation();
+		}
+	}
+}
+
+void UFlexiCameraModeStack::PushCameraMode(TSubclassOf<UFlexiCameraMode> CameraModeClass)
+{
+	if (!CameraModeClass)
+	{
+		return;
+	}
+
+	UFlexiCameraMode* CameraMode = GetCameraModeInstance(CameraModeClass);
+	check(CameraMode);
+
+	int32 StackSize = CameraModeStack.Num();
+
+	if ((StackSize > 0) && (CameraModeStack[0] == CameraMode))
+	{
+		// Already top of stack.
+		return;
+	}
+
+	// See if it's already in the stack and remove it.
+	// Figure out how much it was contributing to the stack.
+	int32 ExistingStackIndex = INDEX_NONE;
+	float ExistingStackContribution = 1.0f;
+
+	for (int32 StackIndex = 0; StackIndex < StackSize; ++StackIndex)
+	{
+		if (CameraModeStack[StackIndex] == CameraMode)
+		{
+			ExistingStackIndex = StackIndex;
+			ExistingStackContribution *= CameraMode->GetBlendWeight();
+			break;
+		}
+		else
+		{
+			ExistingStackContribution *= (1.0f - CameraModeStack[StackIndex]->GetBlendWeight());
+		}
+	}
+
+	if (ExistingStackIndex != INDEX_NONE)
+	{
+		CameraModeStack.RemoveAt(ExistingStackIndex);
+		StackSize--;
+	}
+	else
+	{
+		ExistingStackContribution = 0.0f;
+	}
+
+	// Decide what initial weight to start with.
+	const bool bShouldBlend = ((CameraMode->GetBlendTime() > 0.0f) && (StackSize > 0));
+	const float BlendWeight = (bShouldBlend ? ExistingStackContribution : 1.0f);
+
+	CameraMode->SetBlendWeight(BlendWeight);
+
+	// Add new entry to top of stack.
+	CameraModeStack.Insert(CameraMode, 0);
+
+	// Make sure stack bottom is always weighted 100%.
+	CameraModeStack.Last()->SetBlendWeight(1.0f);
+
+	// Let the camera mode know if it's being added to the stack.
+	if (ExistingStackIndex == INDEX_NONE)
+	{
+		CameraMode->OnActivation();
+	}
+}
+
+bool UFlexiCameraModeStack::EvaluateStack(float DeltaTime, FCameraModeView& OutCameraModeView)
+{
+	if (!bIsActive)
+	{
+		return false;
+	}
+
+	UpdateStack(DeltaTime);
+	BlendStack(OutCameraModeView);
+
+	return true;
+}
+
+void UFlexiCameraModeStack::GetBlendInfo(float& OutWeightOfTopLayer, FGameplayTag& OutTagOfTopLayer) const
+{
+	if (CameraModeStack.Num() == 0)
+	{
+		OutWeightOfTopLayer = 1.0f;
+		OutTagOfTopLayer = FGameplayTag();
+		return;
+	}
+	else
+	{
+		UFlexiCameraMode* TopEntry = CameraModeStack.Last();
+		check(TopEntry);
+		OutWeightOfTopLayer = TopEntry->GetBlendWeight();
+		OutTagOfTopLayer = TopEntry->GetCameraTypeTag();
+	}
+}
+
+UFlexiCameraMode* UFlexiCameraModeStack::GetCameraModeInstance(TSubclassOf<UFlexiCameraMode> CameraModeClass)
+{
+	check(CameraModeClass);
+
+	// First see if we already created one.
+	for (UFlexiCameraMode* CameraMode : CameraModeInstances)
+	{
+		if ((CameraMode != nullptr) && (CameraMode->GetClass() == CameraModeClass))
+		{
+			return CameraMode;
+		}
+	}
+
+	// Not found, so we need to create it.
+	UFlexiCameraMode* NewCameraMode = NewObject<UFlexiCameraMode>(GetOuter(), CameraModeClass, NAME_None, RF_NoFlags);
+	check(NewCameraMode);
+
+	CameraModeInstances.Add(NewCameraMode);
+
+	return NewCameraMode;
+}
+
+void UFlexiCameraModeStack::BlendStack(FCameraModeView& OutCameraModeView) const
+{
+	const int32 StackSize = CameraModeStack.Num();
+	if (StackSize <= 0)
+	{
+		return;
+	}
+
+	// Start at the bottom and blend up the stack
+	const UFlexiCameraMode* CameraMode = CameraModeStack[StackSize - 1];
+	check(CameraMode);
+
+	OutCameraModeView = CameraMode->GetCameraModeView();
+
+	for (int32 StackIndex = (StackSize - 2); StackIndex >= 0; --StackIndex)
+	{
+		CameraMode = CameraModeStack[StackIndex];
+		check(CameraMode);
+
+		OutCameraModeView.Blend(CameraMode->GetCameraModeView(), CameraMode->GetBlendWeight());
+	}
+}
+
+void UFlexiCameraModeStack::UpdateStack(float DeltaTime)
+{
+	const int32 StackSize = CameraModeStack.Num();
+	if (StackSize <= 0)
+	{
+		return;
+	}
+
+	int32 RemoveCount = 0;
+	int32 RemoveIndex = INDEX_NONE;
+
+	for (int32 StackIndex = 0; StackIndex < StackSize; ++StackIndex)
+	{
+		UFlexiCameraMode* CameraMode = CameraModeStack[StackIndex];
+		check(CameraMode);
+
+		CameraMode->UpdateCameraMode(DeltaTime);
+
+		if (CameraMode->GetBlendWeight() >= 1.0f)
+		{
+			// Everything below this mode is now irrelevant and can be removed.
+			RemoveIndex = (StackIndex + 1);
+			RemoveCount = (StackSize - RemoveIndex);
+			break;
+		}
+	}
+
+	if (RemoveCount > 0)
+	{
+		// Let the camera modes know they being removed from the stack.
+		for (int32 StackIndex = RemoveIndex; StackIndex < StackSize; ++StackIndex)
+		{
+			UFlexiCameraMode* CameraMode = CameraModeStack[StackIndex];
+			check(CameraMode);
+
+			CameraMode->OnDeactivation();
+		}
+
+		CameraModeStack.RemoveAt(RemoveIndex, RemoveCount);
+	}
+}
---
+++ Source/FlexiCam/Public/CameraModes/FlexiCameraMode.h
@@ -0,0 +1,187 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "GameplayTagContainer.h"
+
+#include "FlexiCameraMode.generated.h"
+
+#define UE_API FLEXICAM_API
+
+class AActor;
+class UFlexiCameraComponent;
+
+/**
+ * ECameraModeBlendFunction
+ *
+ *	Blend function used for transitioning between camera modes.
+ */
+UENUM(BlueprintType)
+enum class ECameraModeBlendFunction : uint8
+{
+	// Does a simple linear interpolation.
+	Linear,
+
+	// Immediately accelerates, but smoothly decelerates into the target.  Ease amount controlled by the exponent.
+	EaseIn,
+
+	// Smoothly accelerates, but does not decelerate into the target.  Ease amount controlled by the exponent.
+	EaseOut,
+
+	// Smoothly accelerates and decelerates.  Ease amount controlled by the exponent.
+	EaseInOut,
+
+	COUNT	UMETA(Hidden)
+};
+
+/**
+ * FCameraModeView
+ *
+ *	View data produced by the camera mode that is used to blend camera modes.
+ */
+struct FCameraModeView
+{
+public:
+
+	FCameraModeView();
+
+	void Blend(const FCameraModeView& Other, float OtherWeight);
+
+public:
+
+	FVector Location;
+	FRotator Rotation;
+	FRotator ControlRotation;
+	float FieldOfView;
+};
+
+
+/**
+ * Base class for all FlexiCam camera modes
+ */
+UCLASS(MinimalAPI, Abstract, Blueprintable)
+class UFlexiCameraMode : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UE_API UFlexiCameraMode();
+	UE_API UFlexiCameraComponent* GetFlexiCameraComponent() const;
+	UE_API virtual UWorld* GetWorld() const override;
+	UE_API AActor* GetTargetActor() const;
+	UE_API void UpdateCameraMode(float DeltaTime);
+	UE_API void SetBlendWeight(float Weight);
+
+public:
+	// Called when this camera mode is activated on the camera mode stack.
+	virtual void OnActivation() {};
+	// Called when this camera mode is deactivated on the camera mode stack.
+	virtual void OnDeactivation() {};
+
+	const FCameraModeView& GetCameraModeView() const;
+	float GetBlendTime() const;
+	float GetBlendWeight() const;
+	FGameplayTag GetCameraTypeTag() const;
+
+protected:
+
+	UE_API virtual FVector GetPivotLocation() const;
+	UE_API virtual FRotator GetPivotRotation() const;
+
+	UE_API virtual void UpdateView(float DeltaTime);
+	UE_API virtual void UpdateBlending(float DeltaTime);
+
+protected:
+	// A tag that can be queried by gameplay code that cares when a kind of camera mode is active
+	// without having to ask about a specific mode.
+	UPROPERTY(EditDefaultsOnly, Category = "Blending")
+	FGameplayTag CameraTypeTag;
+
+	// View output produced by the camera mode.
+	FCameraModeView View;
+
+	// The horizontal field of view (in degrees).
+	UPROPERTY(EditDefaultsOnly, Category = "View", Meta = (UIMin = "5.0", UIMax = "170", ClampMin = "5.0", ClampMax = "170.0"))
+	float FieldOfView;
+
+	// Minimum view pitch (in degrees).
+	UPROPERTY(EditDefaultsOnly, Category = "View", Meta = (UIMin = "-89.9", UIMax = "89.9", ClampMin = "-89.9", ClampMax = "89.9"))
+	float ViewPitchMin;
+
+	// Maximum view pitch (in degrees).
+	UPROPERTY(EditDefaultsOnly, Category = "View", Meta = (UIMin = "-89.9", UIMax = "89.9", ClampMin = "-89.9", ClampMax = "89.9"))
+	float ViewPitchMax;
+
+	// How long it takes to blend in this mode.
+	UPROPERTY(EditDefaultsOnly, Category = "Blending")
+	float BlendTime;
+
+	// Function used for blending.
+	UPROPERTY(EditDefaultsOnly, Category = "Blending")
+	ECameraModeBlendFunction BlendFunction;
+
+	// Exponent used by blend functions to control the shape of the curve.
+	UPROPERTY(EditDefaultsOnly, Category = "Blending")
+	float BlendExponent;
+
+	// Linear blend alpha used to determine the blend weight.
+	float BlendAlpha;
+
+	// Blend weight calculated using the blend alpha and function.
+	float BlendWeight;
+
+protected:
+	/** If true, skips all interpolation and puts camera in ideal location.  Automatically set to false next frame. */
+	UPROPERTY(transient)
+	uint32 bResetInterpolation : 1;
+};
+
+
+/**
+ * UFlexiCameraModeStack
+ *
+ *	Stack used for blending camera modes.
+ */
+UCLASS()
+class UFlexiCameraModeStack : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	UFlexiCameraModeStack();
+
+	void ActivateStack();
+	void DeactivateStack();
+
+	bool IsStackActivate() const { return bIsActive; }
+
+	void PushCameraMode(TSubclassOf<UFlexiCameraMode> CameraModeClass);
+
+	bool EvaluateStack(float DeltaTime, FCameraModeView& OutCameraModeView);
+
+	// Gets the tag associated with the top layer and the blend weight of it
+	void GetBlendInfo(float& OutWeightOfTopLayer, FGameplayTag& OutTagOfTopLayer) const;
+
+protected:
+
+	UFlexiCameraMode* GetCameraModeInstance(TSubclassOf<UFlexiCameraMode> CameraModeClass);
+
+	void UpdateStack(float DeltaTime);
+	void BlendStack(FCameraModeView& OutCameraModeView) const;
+
+protected:
+
+	bool bIsActive;
+
+	UPROPERTY()
+	TArray<TObjectPtr<UFlexiCameraMode>> CameraModeInstances;
+
+	UPROPERTY()
+	TArray<TObjectPtr<UFlexiCameraMode>> CameraModeStack;
+};
+
+//#undef UE_API
