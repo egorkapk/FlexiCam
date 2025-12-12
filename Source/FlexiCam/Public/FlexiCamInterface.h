3d184b84b518ca1ab872080fe6feb7cebd351ba3
egorkapk <105906309+egorkapk@users.noreply.github.com>
2025-12-12 18:09:21 +0300

    Fix syntax error

--- Source/FlexiCam/Public/FlexiCamInterface.h
+++ Source/FlexiCam/Public/FlexiCamInterface.h
@@ -24,6 +24,6 @@ class FLEXICAM_API IFlexiCamInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "FlexiCamInerface")
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "FlexiCamInterface")
 	UFlexiCameraModeManagerComponent* GetFlexiCameraModeManagerComponent() const;
 };
